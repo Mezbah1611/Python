800747cdbfd17d477ea503b2b970844d29d402cd
Mezbah1611 <mezbahuddin203@gmail.com>
2024-11-12 23:30:42 +0600

    initial commit

---
+++ Exception Handeling.py
@@ -0,0 +1,16 @@
+
+try:
+    list = [20,0,30]
+    result = list[0]/list[3]
+    print(result)
+    print("Done")
+
+except ZeroDivisionError:
+    print("Deviding by zero is not possible")
+
+
+
+finally:
+    
+
+    print("Seccessful")
